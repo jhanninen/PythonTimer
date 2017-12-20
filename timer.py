a23980fcb79365ca7bc1ba1a5a120442e5d944fd
Juho Hänninen <juho.hanninen@hotmail.fi>
2017-12-20 17:41:21 +0200

    Added Timer class

---
+++ timer.py
@@ -0,0 +1,26 @@
+import time
+
+class Timer():
+
+    def __init__(self):
+        self.reset()
+        
+    def reset(self):
+        self.time = time.time()
+        self.paused = False
+        
+    def get_time(self):
+        if not self.paused:
+            return time.time() - self.time
+        else:
+            return self.paused_at - self.time
+        
+    def pause_timer(self):
+        self.paused_at = time.time()
+        self.paused = True
+        
+    def resume_timer(self):
+        if self.paused:
+            self.time += self.paused_at - time.time()
+            self.paused_at = None
+    
